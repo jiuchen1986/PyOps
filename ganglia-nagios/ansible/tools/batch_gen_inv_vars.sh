7f5e858c179966ee6385f42455889f60df10a5fa
Xin Chen <xin.e.chen@ericsson.com>
2018-08-14 18:06:06 +0800

    enhance the invetory and vars file generation

---
+++ ganglia-nagios/ansible/tools/batch_gen_inv_vars.sh
@@ -0,0 +1,103 @@
+#!/bin/python
+
+import argparse
+import os
+import shutil
+
+DEFAULT_SIN_GEN_FILE = "gen_inv_and_vars.sh"
+DEFAULT_OUTPUT_DIR = "/root/ansible/gen_inv_vars"
+DEFAULT_BAS_VARS_FILE = "basic_vars_eccd.yaml"
+DEFAULT_PORTS_FILE = "cluster_gmond_ports"
+DEFAULT_ADD_HOSTS_FILE = "add_hostnames_to_hosts.yaml"
+DEFAULT_GROUP_VARS_DIR = "/root/ansible/group_vars"
+DEFAULT_INVENTORY_DIR = "/root/ansible/inventory"
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-l", "--clusterlist", help="cluster list splitted by comma")
+parser.add_argument("-v", "--basicvars", help="path to the basic vars file, default is \"%s\"" % DEFAULT_BAS_VARS_FILE)
+parser.add_argument("-o", "--outputdir", help="dir storing the output files, default is \"%s\"" % DEFAULT_OUTPUT_DIR)
+parser.add_argument("-s", "--singlegen", help="path to the script file that generate inv and vars for a single cluster, \
+                                               default is \"%s\"" % DEFAULT_SIN_GEN_FILE)
+parser.add_argument("-a", "--addhosts", help="path to the ansible playbook file that adds host mappings \
+                                              to the hosts file on cluster header, default is \"%s\"" % DEFAULT_ADD_HOSTS_FILE)
+parser.add_argument("-g", "--gmondportsfile", help="optional path to a file storing the mappings between cluster name and gmond port, \
+                                                    if this is set, the value of gmondport will be ignored, and a port is generated from \
+                                                    the maximal port indicated in the file plus 1, default is \"%s\"" % DEFAULT_PORTS_FILE)
+parser.add_argument("--groupvars", help="dir to ansible playbook group_vars setting up monitoring system, default is \"%s\"" % DEFAULT_GROUP_VARS_DIR)
+parser.add_argument("--inventory", help="dir to ansible playbook inventory setting up monitoring system, default is \"%s\"" % DEFAULT_INVENTORY_DIR)
+
+
+args = parser.parse_args()
+
+cluster_list = args.clusterlist.split(',')
+
+if args.singlegen:
+  sin_gen_file = os.path.abspath(args.singlegen)
+else:
+  sin_gen_file = os.path.abspath(DEFAULT_SIN_GEN_FILE)
+
+if args.outputdir:
+  abs_output_dir = os.path.abspath(args.outputdir)
+else:
+  abs_output_dir = os.path.abspath(DEFAULT_OUTPUT_DIR)
+
+if args.basicvars:
+  bas_vars_file = os.path.abspath(args.basicvars)
+else:
+  bas_vars_file = os.path.abspath(DEFAULT_BAS_VARS_FILE)
+
+if args.gmondportsfile:
+  ports_file = os.path.abspath(args.gmondportsfile)
+else:
+  ports_file = os.path.abspath(DEFAULT_PORTS_FILE)
+
+if args.addhosts:
+  add_hosts_file = os.path.abspath(args.addhosts)
+else:
+  add_hosts_file = os.path.abspath(DEFAULT_ADD_HOSTS_FILE)
+  
+if args.outputdir:
+  ori_inv_file = os.path.abspath(os.path.join(args.outputdir, 'ori_hosts'))
+else:
+  ori_inv_file = os.path.abspath(os.path.join(DEFAULT_OUTPUT_DIR, 'ori_hosts'))
+
+if args.groupvars:
+  group_vars_dir = os.path.abspath(args.groupvars)
+else:
+  group_vars_dir = os.path.abspath(DEFAULT_GROUP_VARS_DIR)
+
+if args.inventory:
+  inventory_dir = os.path.abspath(args.inventory)
+else:
+  inventory_dir = os.path.abspath(DEFAULT_INVENTORY_DIR)
+
+for c in cluster_list:
+  # fetch original inventory file from target cluster lb
+  cmd_str = "scp root@%s:/home/raket/inventory/hosts %s" % (c, ori_inv_file)
+  if os.system(cmd_str) != 0:
+    os._exit(1)
+
+  # generate inventory and vars file for a single cluster
+  cmd_str = "%s -n %s -v %s -o %s -i %s -t short -g %s" % (sin_gen_file, c, bas_vars_file, \
+                                                           abs_output_dir, ori_inv_file, ports_file)
+  if os.system(cmd_str) != 0:
+    os._exit(1)
+
+  # add hosts info to the hosts file on the cluster header host
+  add_hosts_vars = os.path.join(abs_output_dir, c + "_hosts.yaml")
+  cmd_str = "ansible-playbook -e \"@%s\" %s" % (add_hosts_vars, add_hosts_file)
+  if os.system(cmd_str) != 0:
+    os._exit(1)
+
+  # remove the original inventory file
+  if os.system("rm -rf %s" % ori_inv_file) != 0:
+    os._exit(1)
+
+  # copy vars file to the group_vars dir used by ansible playbook
+  shutil.copyfile(os.path.join(abs_output_dir, c + ".yaml"), os.path.join(group_vars_dir, c + ".yaml"))
+  print("copy %s -> %s" % (os.path.join(abs_output_dir, c + ".yaml"), os.path.join(group_vars_dir, c + ".yaml")))
+  
+  # copy inv file to the inventory dir used by ansible playbook
+  shutil.copyfile(os.path.join(abs_output_dir, c), os.path.join(inventory_dir, c))
+  print("copy %s -> %s" % (os.path.join(abs_output_dir, c), os.path.join(inventory_dir, c)))
+  
--- ganglia-nagios/ansible/tools/gen_inv_and_vars.sh
+++ ganglia-nagios/ansible/tools/gen_inv_and_vars.sh
@@ -4,67 +4,109 @@ import yaml
 import argparse
 import os
 
+global parser, args, header_host, host_map
+
 parser = argparse.ArgumentParser()
 parser.add_argument("-n", "--clustername", help="name of the cluster")
 parser.add_argument("-p", "--gmondport", type=int, help="port the gmond listens on the host")
-parser.add_argument("-c", "--contacts", help="optional cluster contact list splitted by comma, default is Ting Yan,Wei Wang ")
+parser.add_argument("-c", "--contacts", help="optional cluster contact list splitted by comma, default is Ting Yan")
 parser.add_argument("-v", "--basicvars", help="path to the basic vars file")
 parser.add_argument("-o", "--outputdir", help="dir storing the output files")
 parser.add_argument("-i", "--invfile", help="path to the original inventory file")
 parser.add_argument("-t", "--hosttype", help="optional type of hostname appearing in gangali, short or fqdn, default is fqdn")
+parser.add_argument("-g", "--gmondportsfile", help="optional path to a file storing the mappings between cluster name and gmond port, \
+                                                    if this is set, the value of gmondport will be ignored, and a port is generated from \
+                                                    the maximal port indicated in the file plus 1")
 args = parser.parse_args()
 
 header_host = '' # ip of the host as the header of the cluster, default points to master 1
 host_map = {} # ip:hostname
 
 # parse the inventory file
-with open(os.path.abspath(args.invfile), 'r') as inv_ori:
-  for l in inv_ori.readlines():
-    data = l.split()
-    if len(data) >= 3:
-      if 'ansible_host' in data[1]:
-        host_ip = data[1].split('=')[1]
-        host_name = data[2].split('=')[1]
-        host_map[host_ip] = host_name
-        if header_host == '':
-          if ('master1' in host_name) or ((args.clustername + '-1') in host_name):
-            header_host = host_ip
-            print("select %s as header host" % host_name)
-  inv_ori.close()
+def parse_inv():
+  global header_host, host_map, args
+  with open(os.path.abspath(args.invfile), 'r') as inv_ori:
+    for l in inv_ori.readlines():
+      data = l.split()
+      if len(data) >= 3:
+        if 'ansible_host' in data[1]:
+          host_ip = data[1].split('=')[1]
+          host_name = data[2].split('=')[1]
+          host_map[host_ip] = host_name
+          if header_host == '':
+            if ('master1' in host_name) or ((args.clustername + '-1') in host_name):
+              header_host = host_ip
+              print("select %s as header host" % host_name)
+    inv_ori.close()
 
 # generate the new inventory file
-inv_output_file = os.path.join(args.outputdir, args.clustername)
-with open(os.path.abspath(inv_output_file), 'w') as inv_output:
-  inv_output.write("[" + args.clustername + "]\n")
-  for k, v in host_map.items():
-    if 'lb' not in v:
-      if 'node' not in v:
-        inv_output.write(k + "  cluster_role=master\n")
-      else:
-        inv_output.write(k + "  cluster_role=worker\n")
-  inv_output.close()
+def gen_new_inv():
+  global header_host, host_map, args
+  inv_output_file = os.path.join(args.outputdir, args.clustername)
+  with open(os.path.abspath(inv_output_file), 'w') as inv_output:
+    inv_output.write("[" + args.clustername + "]\n")
+    for k, v in host_map.items():
+      if 'lb' not in v:
+        if 'node' not in v:
+          inv_output.write(k + "  cluster_role=master\n")
+        else:
+          inv_output.write(k + "  cluster_role=worker\n")
+    inv_output.close()
 
-# generate the data may need to fill the hosts file at header host
-hosts_output_file = os.path.join(args.outputdir, args.clustername + '_hosts')
-with open(os.path.abspath(hosts_output_file), 'w') as hosts_output:
-  for k, v in host_map.items():
-    if k != header_host:
-      hosts_output.write(k + " " + v + "\n")
-  hosts_output.close()
+# generate the vars file used by add_hostnames_to_hosts.yaml
+def gen_hosts_map():
+  global header_host, host_map, args
+  add_hosts = {}
+  hosts_output_file = os.path.join(args.outputdir, args.clustername + '_hosts.yaml')
+  add_hosts['target'] = header_host
+  add_hosts['host_maps'] = host_map
+  with open(os.path.abspath(hosts_output_file), 'w') as hosts_output:
+    yaml.dump(add_hosts, hosts_output, default_flow_style=False)
+    hosts_output.close()
   
 # generate the vars file
-vars = {}
-with open(os.path.abspath(args.basicvars), 'r') as basic_vars:
-  vars = yaml.load(basic_vars)
-  basic_vars.close()
-vars_output_file = os.path.join(args.outputdir, args.clustername + '.yaml')
-with open(os.path.abspath(vars_output_file), 'w') as vars_output:
-  vars['gmond_multicast_port'] = args.gmondport
-  vars['gmond_cluster_head'] = [header_host + ':{{ gmond_multicast_port }}']
-  vars['nagios_check_ganglia_hostname_type'] = 'fqdn'
-  if args.contacts:
-    vars['cluster_contacts'] = args.contacts.split(',')
-  if args.hosttype:
-    vars['nagios_check_ganglia_hostname_type'] = 'short'
-  yaml.dump(vars, vars_output, default_flow_style=False)
-  vars_output.close()
+def gen_vars():
+  global header_host, host_map, args
+  vars = {}
+  ports = []
+  port_maps = {}
+  with open(os.path.abspath(args.basicvars), 'r') as basic_vars:
+    vars = yaml.load(basic_vars)
+    basic_vars.close()
+  if args.gmondportsfile:
+    with open(os.path.abspath(args.gmondportsfile), 'r') as gmond_ports:
+      for l in gmond_ports.readlines():
+        ports.append(int(l.split()[1]))
+        port_maps[l.split()[0]] = int(l.split()[1])
+      gmond_ports.close()
+  vars_output_file = os.path.join(args.outputdir, args.clustername + '.yaml')
+  with open(os.path.abspath(vars_output_file), 'w') as vars_output:
+    if len(ports) == 0:
+      vars['gmond_multicast_port'] = args.gmondport
+    elif args.clustername in port_maps:
+      vars['gmond_multicast_port'] = port_maps[args.clustername]
+    else:
+      vars['gmond_multicast_port'] = max(ports) + 1
+    print("select %d as gmond port" % vars['gmond_multicast_port'])
+    vars['gmond_cluster_head'] = [header_host + ':{{ gmond_multicast_port }}']
+    vars['nagios_check_ganglia_hostname_type'] = 'fqdn'
+    if args.contacts:
+      vars['cluster_contacts'] = args.contacts.split(',')
+    if args.hosttype:
+      vars['nagios_check_ganglia_hostname_type'] = args.hosttype
+    yaml.dump(vars, vars_output, default_flow_style=False)
+    vars_output.close()
+  
+  # record the mapping between the cluster name and the gmond port
+  if args.gmondportsfile:
+    with open(os.path.abspath(args.gmondportsfile), 'a') as gmond_ports:
+      if args.clustername not in port_maps:
+        gmond_ports.write("%s                  %d\n" % (args.clustername, vars['gmond_multicast_port']))
+      gmond_ports.close()
+
+if __name__ == '__main__':
+
+  parse_inv()
+  gen_new_inv()
+  gen_hosts_map()
+  gen_vars()
